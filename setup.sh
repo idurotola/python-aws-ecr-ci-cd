8bd6b1452a363be4c759cbc8a263a38d1591e05c
Iyanu-Tomiwa <iyanu.durotola@andela.co>
2017-09-20 18:08:37 +0100

    Add the right version to js installation

--- setup.sh
+++ setup.sh
@@ -44,7 +44,7 @@ make_task_def(){
 }
 
 push_ecr_image(){
-  eval $(aws ecr get-login  --no-include-email --region us-west-2)
+  eval $(aws ecr get-login --region us-west-2)
   docker push $AWS_ACCOUNT_ID.dkr.ecr.us-west-2.amazonaws.com/autodeploy:$CIRCLE_SHA1
 }
 
