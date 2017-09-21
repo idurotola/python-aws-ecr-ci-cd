8b5d5eb12997ad787a7f0d36c0fbdba0cd3cfe41
Iyanu-Tomiwa <iyanu.durotola@andela.co>
2017-09-21 05:36:00 +0100

    Added some else to install

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -7,7 +7,8 @@ set -o pipefail
 apk add --no-cache py-pip=9.0.0-r1
 pip install docker-compose==1.12.0 
 pip install awscli==1.11.76
-apt-get install libcurl4-openssl-dev
-pip install pycurl
+tar -zxvf pycurl-7.10.5.tar.gz
+cd pycurl-7.10.5
+python setup.py install
 
 curl -L -o ~/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5rc1/jq-linux-x86_64-static && chmod +x ~/bin/jq
