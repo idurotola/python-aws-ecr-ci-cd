fe3d89e3dfd458f0aa912124168e72cdf04db505
Iyanu-Tomiwa <iyanu.durotola@andela.co>
2017-09-21 06:30:50 +0100

    back to trying curl

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,9 +5,8 @@ set -u
 set -o pipefail
 
 apk add --no-cache py-pip=9.0.0-r1
-apk add jq
-# apk add --update  --no-cache curl
+apk add --update  --no-cache curl
 pip install docker-compose==1.12.0 
 pip install awscli==1.11.76
 
-# curl -L -o ~/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5rc1/jq-linux-x86_64-static && chmod +x ~/bin/jq
+curl -L -o ~/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5rc1/jq-linux-x86_64-static && chmod +x ~/bin/jq
