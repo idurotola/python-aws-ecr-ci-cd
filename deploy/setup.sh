836c4085b1ac16c595563ac87f3bae57384975e0
Iyanu-Tomiwa <iyanu.durotola@andela.co>
2017-09-21 05:19:43 +0100

    Add deploy directory

---
+++ deploy/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+apk add --no-cache py-pip=9.0.0-r1
+pip install docker-compose==1.12.0 
+pip install awscli==1.11.76
+
+curl -L -o ~/bin/jq https://github.com/stedolan/jq/releases/download/jq-1.5rc1/jq-linux-x86_64-static && chmod +x ~/bin/jq
