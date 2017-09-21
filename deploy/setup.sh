787f7ca0e080aae16c1626b46621c911c7124afd
Iyanu-Tomiwa <iyanu.durotola@andela.co>
2017-09-21 06:32:42 +0100

    back to trying curl

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -5,7 +5,7 @@ set -u
 set -o pipefail
 
 apk add --no-cache py-pip=9.0.0-r1
-apk add --update  --no-cache curl
+apk add --update  --no-cache curl curl-dev
 pip install docker-compose==1.12.0 
 pip install awscli==1.11.76
 
